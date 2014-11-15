03ec4862547401d372cd4e5314ace89406c48483
SGStoyanov <stoyanov.vn@gmail.com>
2014-11-15 21:28:54 +0200

    Commit

---
+++ BrideDefence/Towers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrideDefence
+{
+    public abstract class Towers : GameEngine
+    {
+        public int setActive
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ BrideDefence/Witch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrideDefence
+{
+    public class Witch : Monsters
+    {
+    }
+}
